6453f1a36974a406abc833d8c44ba56beda1f8b6
lucafacchini0 <luca.facchini1@protonmail.com>
2025-11-30 19:24:53 +0200

    Cleaned Clode

--- index.js
+++ index.js
@@ -3,20 +3,8 @@ const app = express(); // Crea un'istanza express
 
 const { people } = require('./people');
 
-app.get('/json', (req, res) => {
+app.get('/people', (req, res) => {
     res.json(people)
-})
-
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname + "/public"});
-})
-app.get('/about', (req, res) => {
-    res.send("Contacts page, I'm Luca!")
-})
-app.all(/(.*)/, (req, res) => { // /(.*)/ means: every page
-    res.send("<h1>404 Not Found</h1>")
 })
 
 app.listen(3000);
