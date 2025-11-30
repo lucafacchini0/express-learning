a572624980eb52694fd76649ea8bc563abeadf62
lucafacchini0 <luca.facchini1@protonmail.com>
2025-11-30 19:08:27 +0200

    Add people data and JSON endpoint for retrieving it

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 const express = require('express');
 const app = express(); // Crea un'istanza express
 
+const { people } = require('./people');
 
-// Questo serve a far in modo che, se ad esempio 
-// la pagina index.html richiede style.css, automaticamente 
-// Express lo prende dalla cartella.
+app.get('/json', (req, res) => {
+    res.json(people)
+})
 
-// Ad esempio, se in public ho /public/index.html e /public/style.css, 
-// Express va automaticamente ad usare style.css, senza bisogno della
-// mia autorizzazione.
 app.use(express.static('public'))
-
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: __dirname + "/public"});
 })
-
 app.get('/about', (req, res) => {
     res.send("Contacts page, I'm Luca!")
 })
-
 app.all(/(.*)/, (req, res) => { // /(.*)/ means: every page
     res.send("<h1>404 Not Found</h1>")
 })
-    
+
 app.listen(3000);
---
+++ people.js
@@ -0,0 +1,26 @@
+const people = [
+    {
+        name: "Luca",
+        surname: "Facchini",
+        age: "18",
+        address: {
+            city: "Milan",
+            street: "Top Secret",
+            civic: "18"
+        },
+        interests: ["Gym", "Coding"]
+    },
+    {
+        name: "Anna",
+        surname: "Zuckemberg",
+        age: "138",
+        address: {
+            city: "New York",
+            street: "White House",
+            civic: "1"
+        },
+        interests: ["Hacking", "Walking the dog"]
+    }
+]
+
+module.exports = { people };
