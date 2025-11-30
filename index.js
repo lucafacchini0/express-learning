aca4e7e91c1f431a819fde07bde7211ac21869c9
lucafacchini0 <luca.facchini1@protonmail.com>
2025-11-30 17:51:09 +0200

    Add basic routing for home and about pages with 404 handling

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ app.get('/', (req, res) => {
     res.send("Hello World!");
 })
 
+app.get('/about', (req, res) => {
+    res.send("Contacts page, I'm Luca!")
+})
+
+app.all(/(.*)/, (req, res) => { // /(.*)/ means: every page
+    res.send("<h1>404 Not Found</h1>")
+})
+
 app.listen(3000)
