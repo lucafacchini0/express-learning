076e71e0a458572c35e4f987987db30b96d58c59
lucafacchini0 <luca.facchini1@protonmail.com>
2025-11-30 18:53:09 +0200

    Add static file serving and homepage with basic styling

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express(); // Crea un'istanza express
 
+
+// Questo serve a far in modo che, se ad esempio 
+// la pagina index.html richiede style.css, automaticamente 
+// Express lo prende dalla cartella.
+
+// Ad esempio, se in public ho /public/index.html e /public/style.css, 
+// Express va automaticamente ad usare style.css, senza bisogno della
+// mia autorizzazione.
+app.use(express.static('public'))
+
+
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.sendFile('index.html', {root: __dirname + "/public"});
 })
 
 app.get('/about', (req, res) => {
@@ -12,5 +23,5 @@ app.get('/about', (req, res) => {
 app.all(/(.*)/, (req, res) => { // /(.*)/ means: every page
     res.send("<h1>404 Not Found</h1>")
 })
-
+    
 app.listen(3000);
