9b8492be97ed2bf632261a50ea5ee5701dab6a1f
lucafacchini0 <luca.facchini1@protonmail.com>
2025-12-01 16:27:31 +0200

    Add search endpoint and multiple user entries; refactor middleware

--- users.js
+++ users.js
@@ -22,7 +22,56 @@ const users = [
             civic: "1"
         },
         interests: ["Hacking", "Walking the dog"]
-    }
+    },
+        {
+        id: "3",
+        name: "Alessandro",
+        surname: "Zuckemberg",
+        age: "138",
+        address: {
+            city: "New York",
+            street: "White House",
+            civic: "1"
+        },
+        interests: ["Hacking", "Walking the dog"]
+    },
+        {
+        id: "4",
+        name: "Mirko",
+        surname: "Zuckemberg",
+        age: "138",
+        address: {
+            city: "New York",
+            street: "White House",
+            civic: "1"
+        },
+        interests: ["Hacking", "Walking the dog"]
+    },
+        {
+        id: "5",
+        name: "Matteo",
+        surname: "Zuckemberg",
+        age: "138",
+        address: {
+            city: "New York",
+            street: "White House",
+            civic: "1"
+        },
+        interests: ["Hacking", "Walking the dog"]
+    },
+        {
+        id: "6",
+        name: "Lucas",
+        surname: "Zuckemberg",
+        age: "138",
+        address: {
+            city: "New York",
+            street: "White House",
+            civic: "1"
+        },
+        interests: ["Hacking", "Walking the dog"]
+    },
+
 ]
 
 module.exports = { users };
